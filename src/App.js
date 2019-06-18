4676d9be553581b4c2bf27689f9024008ee67b43
Chenghong Lin <chenghonglin2020@u.northwestern.edu>
2019-06-18 15:17:11 -0500

    Finish slice 6 - fetching data from Firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import "rbx/index.css";
 import {Button, Container, Title} from 'rbx';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 
 const Banner = ({title}) => (
@@ -34,12 +36,13 @@ const hasConflict = (course, selected) => (
     selected.some(selection => courseConflict(course, selection))
 );
 
-const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+const meetsPat = /^(\d\d?):(\d\d)$/;
 
-const timeParts = meets => {
-    const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
-    return !match ? {} : {
-        days,
+const timeParts = course => {
+    const [matchStart, hh1, mm1] = meetsPat.exec(course.start) || [];
+    const [matchsEnd, hh2, mm2] = meetsPat.exec(course.end) || [];
+
+    return {
         hours: {
             start: hh1 * 60 + mm1 * 1,
             end: hh2 * 60 + mm2 * 1
@@ -49,12 +52,12 @@ const timeParts = meets => {
 
 const addCourseTimes = course => ({
     ...course,
-    ...timeParts(course.meets)
+    ...timeParts(course)
 });
 
 const addScheduleTimes = schedule => ({
     title: schedule.title,
-    courses: schedule.courses.map(addCourseTimes)
+    courses: Object.values(schedule.courses).map(addCourseTimes)
 });
 
 const getCourseTerm = course => (
@@ -115,17 +118,28 @@ const CourseList = ({courses}) => {
     );
 };
 
+const firebaseConfig = {
+    apiKey: "AIzaSyB2fXuLpL0-VdZzU6_3_WLWXYb9i5sLezw",
+    authDomain: "course-scheduler-3d94d.firebaseapp.com",
+    databaseURL: "https://course-scheduler-3d94d.firebaseio.com",
+    projectId: "course-scheduler-3d94d",
+    storageBucket: "course-scheduler-3d94d.appspot.com",
+    messagingSenderId: "535620491738",
+    appId: "1:535620491738:web:0d567fb4b08c0a8f"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
+
 const App = () => {
     const [schedule, setSchedule] = useState({title: '', courses: []});
-    const url = 'https://www.cs.northwestern.edu/academics/courses/394/data/cs-courses.php';
+
     useEffect(() => {
-        const fetchSchedule = async () => {
-            const response = await fetch(url);
-            if (!response.ok) throw response;
-            const json = await response.json();
-            setSchedule(addScheduleTimes(json));
+        const handleData = snap => {
+            if(snap.val()) setSchedule(addScheduleTimes(snap.val()));
         };
-        fetchSchedule();
+        db.on('value', handleData, error => alert(error));
+        return () => {db.off('value', handleData);};
     }, []);
 
     return (
